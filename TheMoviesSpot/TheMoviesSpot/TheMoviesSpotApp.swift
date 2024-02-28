b5d2c7852babc664e54157b5315d57e6d3f88532
Alexander Grigoryev <a.a.grigoryev@icloud.com>
2024-02-28 17:06:57 +0400

    Initial project commit

---
+++ TheMoviesSpot/TheMoviesSpot/TheMoviesSpotApp.swift
@@ -0,0 +1,32 @@
+//
+//  TheMoviesSpotApp.swift
+//  TheMoviesSpot
+//
+//  Created by Alexander Grigoryev on 2024-02-28.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TheMoviesSpotApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
